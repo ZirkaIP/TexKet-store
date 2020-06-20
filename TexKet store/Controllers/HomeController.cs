13d0b96a3693f718d50fa8ddd2aba978cef57e6c
Ilia <i.pulman.official@gmail.com>
2020-06-20 15:43:29 +0300

    Добавьте файлы проекта.

---
+++ TexKet store/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace TexKet_store.Controllers
+{
+	public class HomeController:Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ TexKet store/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TexKet_store.DAL.Models;
+using TexKet_store.ViewModels;
+
+namespace TexKet_store.ViewModels
+{
+	public class HomeViewModel
+	{
+		private IEnumerable<Laptop> FavoriteLaptops { get; set; }
+	}
+}
